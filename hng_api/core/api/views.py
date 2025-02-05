fd53d691a8137aa2876c99f96f0146223755de6d
Theophilus Awe <awetheophiluso.1@gmail.com>
2025-02-05 22:06:47 +0100

    Initialize project

---
+++ hng_api/core/api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.views import View
+from django.utils.timezone import now
+
+def api_view(request):
+  return JsonResponse({
+    'email': "awetheophiluso.1@gmail.com",
+    'current_datetime': now().isoformat(),
+    'github_url': 'https://github.com/theophilusawe'
+  })
